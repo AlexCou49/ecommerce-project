cccf4dd06d6e989a149c354225a0764e98e69bc3
Alexandre Coutant <alexandrecoutant@airdealexandre.home>
2022-12-14 14:17:43 +0100

    ok

---
+++ src/components/Shop/Shop.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ShopProductList :products="products"/>
+  </div>
+</template>
+    
+<script setup lang="ts">
+import type { ProductInterface } from '@/interfaces/product.interface';
+import ShopProductList from './ShopProductList.vue';
+
+defineProps<{
+  products:ProductInterface[]
+}>()
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+    
---
+++ src/components/Shop/ShopProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid p-20">
+    <ShopProduct v-for="product in products" :product="product"/>
+  </div>
+</template>
+      
+<script setup lang="ts">
+import type { ProductInterface } from '@/interfaces/product.interface';
+import ShopProduct from './ShopProduct.vue';
+
+defineProps<{
+  products: ProductInterface[]
+}>()
+</script>
+  
+<style lang="scss" scoped>
+.grid{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: 400px;
+  gap:20px;
+}
+</style>
+      
